cc2f97a86b2f9854838150fb3792e5fbf87b3e06
tanzilasattar <maliktanzela71@gmail.com>
2025-02-17 23:08:45 -0800

    update menuitems

--- app/components/MenuItems.js
+++ app/components/MenuItems.js
@@ -38,10 +38,10 @@ const MenuItems = ({ activeCategory, setActiveCategory }) => {
                         ref={el => categoryRefs.current[index] = el} 
                         className="my-2"
                     >
-                        <h2 className="text-2xl font-bold mb-4 text-black dark:text-white">{category.name}</h2>
+                        <h2 className="text-2xl font-bold mb-4">{category.name}</h2>
                         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
                             {category.items.map((item) => (
-                                <div key={item.id} className="group rounded-lg overflow-hidden hover:shadow-lg transition-shadow duration-300 flex flex-col bg-white dark:bg-gray-800">
+                                <div key={item.id} className="group rounded-lg overflow-hidden hover:shadow-lg transition-shadow duration-300 flex flex-col bg-white">
                                     <div className="relative w-full">
                                         <img
                                             src={item.image}
@@ -51,7 +51,7 @@ const MenuItems = ({ activeCategory, setActiveCategory }) => {
                                         />
                                     </div>
                                     <div className="p-4 bg-gray-100 flex-1 flex flex-col min-h-[200px]">
-                                        <h1 className="text-md font-bold line-clamp-2 py-2 text-black dark:text-white">
+                                        <h1 className="text-md font-bold line-clamp-2 py-2">
                                             {item.name}
                                         </h1>
                                         <p className="text-gray-600 text-sm mt-1 line-clamp-2 min-h-[40px]">
